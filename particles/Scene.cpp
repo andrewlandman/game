7797ca818d3c22f622da910a94fa92ef521e5a9e
Andrew Landman <and.landman@gmail.com>
2022-08-14 03:07:12 -0400

    Add quaternion.

--- particles/Scene.cpp
+++ particles/Scene.cpp
@@ -1,4 +1,6 @@
 #include "Scene.hpp"
+#include "Quaternion.hpp"
+#include <iostream>
 
 Scene::Scene(scene::ISceneManager* sceneManager)
     :
@@ -10,12 +12,12 @@ void Scene::initialize() {
     mNode1 = mSceneManager->addSphereSceneNode(5);
     mNode1->setMaterialFlag(video::EMF_LIGHTING, true);
     mNode1->getMaterial(0).EmissiveColor.set(255, 255, 0, 0);
-    mNode1->setPosition(core::vector3df(100, 0, 0));
+    mNode1->setPosition(core::vector3df(0, 0, 0));
 
     mNode2 = mSceneManager->addSphereSceneNode(5);
     mNode2->setMaterialFlag(video::EMF_LIGHTING, true);
     mNode2->getMaterial(0).EmissiveColor.set(255, 0, 0, 255);
-    mNode2->setPosition(core::vector3df(-100, 0, 0));
+    mNode2->setPosition(core::vector3df(100, 0, 0));
 
     // Add cube
     scene::ISceneNode* cube = mSceneManager->addCubeSceneNode();
@@ -41,11 +43,13 @@ void Scene::initialize() {
 }
 
 void Scene::update(uint32_t dt) {
-    core::vector3df pos1 = mNode1->getPosition();
-    pos1.X -= 0.01*dt;
-    mNode1->setPosition(pos1);
-
     core::vector3df pos2 = mNode2->getPosition();
-    pos2.X += 0.01*dt;
+
+    auto q = Quaternion::fromAxisAngle(0.1, 0, 1, 0);
+    q = q->rotate(pos2.X, pos2.Y, pos2.Z);
+    pos2.X = q->mX;
+    pos2.Y = q->mY;
+    pos2.Z = q->mZ;
+
     mNode2->setPosition(pos2);
 }
